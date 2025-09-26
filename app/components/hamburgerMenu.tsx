eb6d0e70b779c9ef63e966e80f66484abbccc985
Inge Bothma <ingesaunders@gmail.com>
2025-09-26 20:53:54 +0100

    Added hamburgermenu component

---
+++ app/components/hamburgerMenu.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+
+export default function HamburgerMenu() {
+  return (
+    <div className="px-4">
+      <MenuOutlinedIcon fontSize="large" />
+    </div>
+  );
+}
