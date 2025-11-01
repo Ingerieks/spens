c902d99a8ac13389d7d73c308e751c06603f8885
Inge Bothma <ingesaunders@gmail.com>
2025-11-01 17:00:39 +0000

    initiated prisma schemas and migrations

---
+++ prisma/migrations/20251101125057_add_recipe/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" SERIAL NOT NULL,
+    "labels" TEXT,
+    "shared" BOOLEAN NOT NULL,
+    "groceries" BOOLEAN NOT NULL,
+    "mealPlan" BOOLEAN NOT NULL,
+    "ingredients" TEXT[],
+    "instructions" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
